e48553c21ce4ea4d17de785bc66dae11db06fe56
unknown <hkkarki01@gmail.com>
2024-11-21 06:22:33 +0545

    list

---
+++ day9.py
@@ -0,0 +1,61 @@
+# identity operator
+# is
+# is not
+
+#membership operator
+# in 
+# not in
+
+"a"=="b"
+"a" is "b"
+
+print( 1 is 2)
+print (5 is 5)
+
+# python data types
+# list , dict , set , tuple
+
+# list
+a=[2, 4 , 5 , 5, 6 , 8]
+print(a)
+print(type(a))
+a[0]=10
+print(a)
+print(a[-2])
+print(a)
+
+# slicing
+print(a[0:4])
+
+print(len(a))
+
+# add item on list
+
+
+b=["hk","krishna"]
+b.append("karki")
+print(b)
+print(len(b))
+
+# insert 
+b.insert(0,"insert")
+b.insert(2,"hello")
+print(b)
+
+
+#concatenate
+a=[1 , 2 , 3 , 4 , 5]
+b=[7 , 8 , 8 , 10]
+print(a+b)
+
+#extends
+
+b.extend(a)
+print(b)
+
+a= [1, 2 ,"hk", "hello", [1,2,3]]
+print(a[4][2])
+
+
+
+
